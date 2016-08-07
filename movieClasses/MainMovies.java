27a8fa43a6a06d2746de929776db844d02d0127e
apennybacker@gmail.com <apennybacker@gmail.com>
2016-08-07 11:54:16 -0400

    added valid input check

--- movieClasses/MainMovies.java
+++ movieClasses/MainMovies.java
@@ -1,3 +1,5 @@
+//Group members:  Karen, Vonnita & Amy
+
 package movieClasses;
 
 import java.util.ArrayList;
@@ -11,73 +13,57 @@ public class MainMovies {
 		String title = "";
 		String choice = "y";
 
-		while (choice.equalsIgnoreCase("y")) {
-
-			
-		ArrayList<Movies> movieList = new ArrayList();
-		
-		movieList.add(new Movies("Frozen", "Animated"));
-		movieList.add(new Movies("Sponge Bob", "Animated"));
-		movieList.add(new Movies("Hotel Transylvania", "Animated"));
-		movieList.add(new Movies("The Green Mile", "Drama"));
-		movieList.add(new Movies("Castaway ", "Drama"));
-		movieList.add(new Movies("Captain Phillips ", "Drama"));
-		movieList.add(new Movies("Friday the 13th", "Horror"));
-		movieList.add(new Movies("Halloween ", "Horror"));
-		movieList.add(new Movies("Chuckie", "Horror"));
-		movieList.add(new Movies("Harry Potter", "SciFi"));
-		movieList.add(new Movies("Star Trek", "SciFi"));
-		movieList.add(new Movies("Planet of the Apes", "SciFi"));
-		
 		System.out.println("Welcome to the Movie List Application!");
 		System.out.println();
 		System.out.println("There are 12 movies in this list.");
-		System.out.print("Which category are you interested in? (Animated/Drama/Horror/SciFi):");
-		
-		category = scan1.nextLine();
-		
-		System.out.println();
-		System.out.println("Here are the movies available in the " + category + " category:");
-		
-		Movies m1 = new Movies(title, category);
-		//m1.setTitle(title);
-		//m1.setCategory(category);
-		m1.setCategory(scan1.toString());
-		
-		//simplifying multiple if statements into a for loop
-		for (int a=0;a< movieList.size(); a++){
-			if(category.equalsIgnoreCase(movieList.get(a).getCategory())){
-			System.out.println(movieList.get(a).getTitle());	
+
+		while (choice.equalsIgnoreCase("y")) {
+
+			ArrayList<Movies> movieList = new ArrayList();
+
+			movieList.add(new Movies("Frozen", "Animated"));
+			movieList.add(new Movies("Sponge Bob", "Animated"));
+			movieList.add(new Movies("Hotel Transylvania", "Animated"));
+			movieList.add(new Movies("The Green Mile", "Drama"));
+			movieList.add(new Movies("Castaway ", "Drama"));
+			movieList.add(new Movies("Captain Phillips ", "Drama"));
+			movieList.add(new Movies("Friday the 13th", "Horror"));
+			movieList.add(new Movies("Halloween ", "Horror"));
+			movieList.add(new Movies("Chuckie", "Horror"));
+			movieList.add(new Movies("Harry Potter", "SciFi"));
+			movieList.add(new Movies("Star Trek", "SciFi"));
+			movieList.add(new Movies("Planet of the Apes", "SciFi"));
+
+			System.out.print("Which category are you interested in? (Animated/Drama/Horror/SciFi):");
+			category = scan1.nextLine();
+
+			System.out.println();
+
+			Movies m1 = new Movies(title, category);
+			// **don't entirely understand the need for this if category is already initialized to a String:
+			m1.setCategory(scan1.toString());
+
+			// adding valid input check
+			if (category.equalsIgnoreCase("animated") || category.equalsIgnoreCase("horror")
+					|| category.equalsIgnoreCase("drama") || category.equalsIgnoreCase("scifi")) {
+				System.out.println("Here are the movies available in the " + category + " category:");
+
+				// simplifying multiple if statements into a for loop...see github history
+				for (int a = 0; a < movieList.size(); a++) {
+					if (category.equalsIgnoreCase(movieList.get(a).getCategory())) {
+						System.out.println(movieList.get(a).getTitle());
+					}
+				}
+			} else {
+				System.out.println("You have entered an invalid option.  Please select a valid choice.");
+				continue;
 			}
-			}
-		
-		/*if(category.equalsIgnoreCase("animated")){
-			System.out.println(movieList.get(0).getTitle());
-			System.out.println(movieList.get(1).getTitle());
-			System.out.println(movieList.get(2).getTitle());
+
+			System.out.println();
+			System.out.print("Would you like to select from a different category? (y/n)");
+			choice = scan1.nextLine();
 		}
-		if(category.equalsIgnoreCase("drama")){
-			System.out.println(movieList.get(3).getTitle());
-			System.out.println(movieList.get(4).getTitle());
-			System.out.println(movieList.get(5).getTitle());
-		}
-		if(category.equalsIgnoreCase("horror")){
-			System.out.println(movieList.get(6).getTitle());
-			System.out.println(movieList.get(7).getTitle());
-			System.out.println(movieList.get(8).getTitle());
-		}
-		if(category.equalsIgnoreCase("scifi")){
-			System.out.println(movieList.get(9).getTitle());
-			System.out.println(movieList.get(10).getTitle());
-			System.out.println(movieList.get(11).getTitle());
-		}
-		*/
-		
-		System.out.println();
-		System.out.println("Would you like to select from a different category? (y/n)");
-		choice = scan1.nextLine();
-	}
-		
+		scan1.close();
 	}
 
 }
